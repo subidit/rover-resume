c1a20bf3522d4de11f337d54ed2bdfefbf1473e3
Rolandas Griskevicius <rol.griskevicius@gmail.com>
2024-11-24 12:11:14 +0200

    defining vars

--- local_run.sh
+++ local_run.sh
@@ -1,1 +1,2 @@
-docker run --rm -t --user="$(id -u):$(id -g)" --net=none -v "$(pwd):/tmp" leplusorg/latex latexmk -outdir=/tmp -pdf /tmp/$1
+#docker run --rm -t --user="$(id -u):$(id -g)" --net=none -v "$(pwd):/tmp" leplusorg/latex latexmk -outdir=/tmp -pdf /tmp/$1
+docker run --rm -t --user="$(id -u):$(id -g)" --net=none -v "$(pwd):/tmp" ghcr.io/xu-cheng/texlive-full latexmk -outdir=/tmp -pdf /tmp/$1
